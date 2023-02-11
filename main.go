0fee6719c43af60e9254d7564652b7f171e3cff7
lebrancconvas <lebrancconvas@gmail.com>
2023-02-11 11:15:10 +0700

    ✨ feat: update

--- main.go
+++ main.go
@@ -8,8 +8,26 @@ func main() {
 	app := fiber.New();
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello Fiber.");
+		return c.SendString("GET: Hello Fiber.");
 	})
-	
+
+	app.Post("/", func(c *fiber.Ctx) error {
+		return c.SendString("POST: Hello Fiber.");
+	})
+
+	// Parameter and Parameter Optional. 
+	app.Get("/hello/:name/:surname?", func(c *fiber.Ctx) error {
+		name := c.Params("name");
+		surname := c.Params("surname");
+		return c.SendString("Name: " + name + " " + surname);
+	})
+
+	// Query 
+	app.Get("/hi", func(c *fiber.Ctx) error {
+		name := c.Query("name");
+		surname := c.Query("surname");
+		return c.SendString("Hi " + name + " " + surname); 
+	})
+
 	app.Listen(":8009"); 
 }
